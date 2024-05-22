c1ccca8d9ee5fd6f187d3395b449be2956015111
Alexander Brefeld <alexander.brefeld@protonmail.com>
2024-05-22 17:27:44 -0500

    working login

--- contacts/settings.py
+++ contacts/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "account.apps.AccountConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -75,8 +76,15 @@ WSGI_APPLICATION = "contacts.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+                "options": "-c search_path=contacts"
+            },
+        "NAME": "contactxdb",
+        "USER": "py_manager",
+        "PASSWORD": "pickeLadvarKtan-gO",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
