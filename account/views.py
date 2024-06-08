1141486d2dec02cf5b442259a88506a363188636
Alexander Brefeld <alexander.brefeld@protonmail.com>
2024-06-08 06:07:41 -0500

    minor changes to dashboard view backend

--- account/views.py
+++ account/views.py
@@ -26,30 +26,20 @@ from .forms import (
 
 @login_required
 def dashboard(request):
-    emails = EmailAddress.objects.filter(user=request.user)
-    phones = Phone.objects.filter(user=request.user)
-    addresses = PostalAddress.objects.filter(user=request.user)
-    socials = SocialProfile.objects.filter(user=request.user)
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard',
-                   'emails': emails,
-                   'phones': phones,
-                   'addresses': addresses,
-                   'socials': socials})
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
 
 
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password'])
-            # Save the User object
             new_user.save()
-            # Create the user profile
             Profile.objects.create(user=new_user)
             return render(
                 request,
@@ -147,66 +137,6 @@ class EditDashboardView(TemplateResponseMixin, View):
              'address_formset': address_formset,
              'socials_formset': socials_formset}
         )
-
-
-@login_required
-def edit(request):
-    if request.method == 'POST':
-        user_form = UserEditForm(
-            instance=request.user,
-            data=request.POST
-        )
-        profile_form = ProfileEditForm(
-            instance=request.user.profile,
-            data=request.POST,
-            files=request.FILES,
-        )
-        email_formset = EmailAddressFormSet(
-            queryset=EmailAddress.objects.filter(user=request.user),
-            instance=request.user.email_addresses,
-            data=request.POST
-        )
-        phone_formset = PhoneFormSet(
-            queryset=Phone.objects.filter(user=request.user),
-            data=request.POST
-        )
-        address_formset = PhoneFormSet(
-            queryset=PostalAddress.objects.filter(user=request.user),
-            data=request.POST
-        )
-        if (
-            user_form.is_valid()
-            and profile_form.is_valid()
-            and email_formset.is_valid()
-            and phone_formset.is_valid()
-            and address_formset.is_valid()
-        ):
-            user_form.save()
-            profile_form.save()
-            email_formset.save()
-            phone_formset.save()
-            address_formset.save()
-            messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Error updating profile')
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
-        email_formset = EmailAddressFormSet(
-            # queryset=EmailAddress.objects.filter(user=request.user)
-            instance=request.user.email_addresses
-        )
-        phone_formset = PhoneFormSet(queryset=Phone.objects.filter(user=request.user))
-        address_formset = PostalAddressFormSet(queryset=PostalAddress.objects.filter(user=request.user))
-    return render(
-        request,
-        'account/edit.html',
-        {'user_form': user_form,
-         'profile_form': profile_form,
-         'email_formset': email_formset,
-         'phone_formset': phone_formset,
-         'address_formset': address_formset}
-    )
 
 
 @login_required
