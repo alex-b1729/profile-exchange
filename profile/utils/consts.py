67f47d3a01246256fb7736f39f8da328a2e4232e
Alexander Brefeld <alexander.brefeld@protonmail.com>
2024-10-03 13:00:56 -0500

    experimental ContentCategories class of dataclasses

--- profile/utils/consts.py
+++ profile/utils/consts.py
@@ -1,4 +1,6 @@
 import os.path
+from abc import ABC
+from dataclasses import dataclass
 
 
 PROFILE_PHOTO_DIR = os.path.join('users', 'profile', 'photo')
@@ -22,3 +24,48 @@ CONTENT_CATEGORIES = {
         'GitHub'
     ],
 }
+
+
+class ContentCategories:
+    categories = [
+        'ContactInfo',
+        'Link',
+    ]
+
+    @dataclass
+    class Category:
+        display_name: str
+        is_app: bool
+        contents: list
+
+        @property
+        def name(self):
+            return ''.join(self.display_name.split())
+
+        def __iter__(self):
+            return iter(self.contents)
+
+        def __len__(self):
+            return len(self.contents)
+
+    def __iter__(self):
+        return (getattr(self, c) for c in self.categories)
+
+    ContactInfo = Category(
+        display_name='Contact Info',
+        is_app=False,
+        contents=[
+            'Email',
+            'Phone',
+            'Address',
+        ],
+    )
+
+    Link = Category(
+        display_name='Link',
+        is_app=True,
+        contents=[
+            'Website',
+            'GitHub',
+        ]
+    )
